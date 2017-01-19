cb758e27ab4c8b7b81b28a6ebdf1a7c340c2ab8f
Wesam Elshiekh <wesam.elshiekh@healthstream.com>
2017-01-19 16:20:19 -0600

    Bug#10: Unable to configure TCPSink through App/web.config in case of using IP address

--- Serilog.Sinks.Network/NetworkLoggerConfigurationExtensions.cs
+++ Serilog.Sinks.Network/NetworkLoggerConfigurationExtensions.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Net;
 using Serilog.Configuration;
+using Serilog.Debugging;
 using Serilog.Events;
 using Serilog.Sinks.Network.Sinks.TCP;
 using Serilog.Sinks.Network.Sinks.UDP;
@@ -9,27 +10,17 @@ using Serilog.Sinks.Network.Sinks.UDP;
 namespace Serilog.Sinks.Network
 {
     /// <summary>
-    /// Extends Serilog configuration to write events to the network.
+    ///     Extends Serilog configuration to write events to the network.
     /// </summary>
     public static class NetworkLoggerConfigurationExtensions
     {
-        private static IPAddress ToIP(string uri)
-        {
-            var ipHostEntry = Dns.GetHostEntry(uri);
-            if (!ipHostEntry.AddressList.Any())
-            {
-                throw new ArgumentException("Could not resolve " + uri);
-            }
-            return ipHostEntry.AddressList.First();
-        }
-
         public static LoggerConfiguration UDPSink(
             this LoggerSinkConfiguration loggerConfiguration,
-            string uri, 
+            string uri,
             int port,
             LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum)
         {
-            var sink = new UDPSink(ToIP(uri), port);
+            var sink = new UDPSink(ResolveAddress(uri), port);
             return loggerConfiguration.Sink(sink, restrictedToMinimumLevel);
         }
 
@@ -57,12 +48,44 @@ namespace Serilog.Sinks.Network
 
         public static LoggerConfiguration TCPSink(
             this LoggerSinkConfiguration loggerConfiguration,
-            string uri, 
+            string uri,
             int port,
             LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum)
         {
-            var sink = new TCPSink(ToIP(uri), port);
+            var sink = new TCPSink(ResolveAddress(uri), port);
             return loggerConfiguration.Sink(sink, restrictedToMinimumLevel);
+        }
+
+        private static IPAddress ResolveAddress(string uri)
+        {
+            // Check if it is IP address
+            IPAddress address;
+
+            if (IPAddress.TryParse(uri, out address))
+                return address;
+
+            address = ResolveIP(uri);
+            if (address != null)
+                return address;
+
+            SelfLog.WriteLine("Unable to determine the destination IP-Address");
+            return IPAddress.Loopback;
+        }
+
+        private static IPAddress ResolveIP(string uri)
+        {
+            try
+            {
+                var ipHostEntry = Dns.GetHostEntry(uri);
+                if (!ipHostEntry.AddressList.Any())
+                    return null;
+                return ipHostEntry.AddressList.First();
+            }
+            catch (Exception)
+            {
+                SelfLog.WriteLine("Could not resolve " + uri);
+                return null;
+            }
         }
     }
 }
