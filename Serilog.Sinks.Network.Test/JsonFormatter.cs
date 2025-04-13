706dea3ddc9383b0e93e06d4d1ac0991ce754dcb
tbdrake <draketb@gmail.com>
2025-04-13 10:03:52 -0400

    Add failing test

--- Serilog.Sinks.Network.Test/JsonFormatter.cs
+++ Serilog.Sinks.Network.Test/JsonFormatter.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Serilog.Core;
+using Serilog.Events;
 using Serilog.Formatting;
 using Serilog.Sinks.Network.Formatters;
 using Xunit;
@@ -12,15 +15,19 @@ namespace Serilog.Sinks.Network.Test
 {
     public class JsonFormatter
     {
-        private static LoggerAndSocket ConfigureTestLogger(ITextFormatter? formatter = null)
+        private static LoggerAndSocket ConfigureTestLogger(ITextFormatter? formatter = null, ILogEventEnricher[]? enrichers = null)
         {
             var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             socket.Bind(new IPEndPoint(IPAddress.Loopback, 0));
             socket.Listen();
 
-            var logger = new LoggerConfiguration()
-                .WriteTo.TCPSink(IPAddress.Loopback, ((IPEndPoint)socket.LocalEndPoint!).Port, null, null, formatter)
-                .CreateLogger();
+            var loggerConfiguration = new LoggerConfiguration()
+                .WriteTo.TCPSink(IPAddress.Loopback, ((IPEndPoint)socket.LocalEndPoint!).Port, null, null, formatter);
+            if (enrichers != null)
+            {
+                loggerConfiguration.Enrich.With(enrichers);
+            }
+            var logger = loggerConfiguration.CreateLogger();
             
             return new LoggerAndSocket { Logger = logger, Socket = socket };
         }
@@ -84,6 +91,44 @@ namespace Serilog.Sinks.Network.Test
 
             receivedData.Should().NotContain("\"traceId\"");
             receivedData.Should().NotContain("\"spanId\"");
+        }
+
+        [Fact]
+        public async Task DoesNotAddDuplicateTraceAndSpanIds()
+        {
+            using var activitySource = new ActivitySource("TestSource");
+            using var activityListener = CreateAndAddActivityListener(activitySource.Name);
+            using Activity? activity = activitySource.StartActivity();
+            Assert.NotNull(activity);
+
+            using var fixture = ConfigureTestLogger(
+                new LogstashJsonFormatter(),
+                // This enricher will add traceId and spanId properties to the log event: 
+                [ new TraceAndSpanEnricher() ]
+            );
+            
+            fixture.Logger.Information("arbitraryMessage");
+
+            var receivedData = await ServerPoller.PollForReceivedData(fixture.Socket);
+
+            // Count the occurrences of traceId and spanId in the received data:
+            var traceIdCount = receivedData.Split("\"traceId\"").Length - 1;
+            traceIdCount.Should().Be(1, "traceId should only appear once in the log message.");
+            var spanIdCount = receivedData.Split("\"spanId\"").Length - 1;
+            spanIdCount.Should().Be(1, "spanId should only appear once in the log message.");
+        }
+
+        private class TraceAndSpanEnricher : ILogEventEnricher
+        {
+            public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+            {
+                var currentActivity = Activity.Current;
+                if (currentActivity != null)
+                {
+                    logEvent.AddOrUpdateProperty(propertyFactory.CreateProperty("traceId", currentActivity.TraceId.ToString()));
+                    logEvent.AddOrUpdateProperty(propertyFactory.CreateProperty("spanId", currentActivity.SpanId.ToString()));
+                }
+            }
         }
 
         private static ActivityListener CreateAndAddActivityListener(string sourceName)
